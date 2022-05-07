40a58c3f5179269abfa1abf5c25102c10eb0cacd
Potasya <yakovlevamava@gmail.com>
2022-05-07 16:18:35 +0200

    Navigation and App Layout

--- src/main/java/com/example/application/data/entity/Company.java
+++ src/main/java/com/example/application/data/entity/Company.java
@@ -1,5 +1,7 @@
 package com.example.application.data.entity;
 
+import org.hibernate.annotations.Formula;
+
 import java.util.LinkedList;
 import java.util.List;
 import javax.annotation.Nullable;
@@ -16,6 +18,9 @@ public class Company extends AbstractEntity {
     @Nullable
     private List<Contact> employees = new LinkedList<>();
 
+    @Formula("(select count(c.id) from Contact c where c.company_id = id)")
+    private int employeeCount;
+
     public String getName() {
         return name;
     }
@@ -30,6 +35,10 @@ public class Company extends AbstractEntity {
 
     public void setEmployees(List<Contact> employees) {
         this.employees = employees;
+    }
+
+    public int getEmployeeCount(){
+        return employeeCount;
     }
 
 }
