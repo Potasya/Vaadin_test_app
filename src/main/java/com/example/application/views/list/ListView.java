d185392e3acffe6783178322456440c0573bb230
Potasya <yakovlevamava@gmail.com>
2022-05-07 18:00:45 +0200

    Unit and Integration Testing

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -12,12 +12,15 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.context.annotation.Scope;
 
 import javax.annotation.security.PermitAll;
 
 @PermitAll
 @Route(value="", layout = MainLayout.class)
 @PageTitle("Contacts | Vaadin CRM")
+@org.springframework.stereotype.Component
+@Scope("prototype")
 public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
